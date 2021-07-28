358776137c768e951f00fc9fbe4ba4632bd8ab0e
razvanWebDev <crsn_razvan@yahoo.com>
2021-07-28 21:59:41 +0300

    small updates

--- admin/admin.php
+++ admin/admin.php
@@ -1,88 +1,69 @@
 <?php include "includes/header.php"; ?>
-  <div class="wrapper">
+<div class="wrapper">
 
-    <?php include "includes/top_navbar.php"; ?>
-    <?php include "includes/sidebar.php"; ?>
+  <?php include "includes/top_navbar.php"; ?>
+  <?php include "includes/sidebar.php"; ?>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
     <?php $page_title = "Home"; ?>
     <?php include "includes/page_title.php"; ?>
 
-      <!-- Main content -->
-      <section class="content">
-        <div class="container-fluid">
-          <!-- Small boxes (Stat box) -->
-          <div class="row">
-            <div class="col-lg-3 col-6">
-              <!-- small box -->
-              <div class="small-box bg-info">
-                <div class="inner">
-                  <h3>150</h3>
-
-                  <p>New Orders</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-primary">
+              <div class="inner">
+                <?php
+                    $query = "SELECT * FROM users";
+                    $select_users = mysqli_query($connection, $query);
+                    $num_users = mysqli_num_rows($select_users);
+                    ?>
+                <h3>
+                  <?php echo $num_users ?>
+                </h3>
+                <p>Admin Users</p>
               </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-              <!-- small box -->
-              <div class="small-box bg-success">
-                <div class="inner">
-                  <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                  <p>Bounce Rate</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <div class="icon">
+                <i class="fas fa-user"></i>
               </div>
+              <a href="users.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-              <!-- small box -->
-              <div class="small-box bg-warning">
-                <div class="inner">
-                  <h3>44</h3>
-
-                  <p>User Registrations</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-person-add"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-              <!-- small box -->
-              <div class="small-box bg-danger">
-                <div class="inner">
-                  <h3>65</h3>
-
-                  <p>Unique Visitors</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-pie-graph"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-            <!-- ./col -->
           </div>
-          <!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </section>
-      <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
 
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <?php
+                  $query = "SELECT * FROM contact";
+                  $result = mysqli_query($connection, $query);
+                  $num_items = mysqli_num_rows($result);
+                  ?>
+                <h3>
+                  <?php echo $num_items ?>
+                </h3>
+                <p>Total Messages</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-envelope"></i>
+              </div>
+              <a href="contact.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
   </div>
-  <!-- ./wrapper -->
+  <!-- /.content-wrapper -->
+
+</div>
+<!-- ./wrapper -->
 
 <?php include "includes/footer.php"; ?>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,7 +1,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="#" class="brand-link">
+      <a href="../" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
         <span class="brand-text font-weight-light">Brand logo</span>
@@ -29,8 +29,9 @@
                with font-awesome or any other icon font library -->
             <li class="nav-item sidebar-page-header" id="home-page-header">
               <a href="#" class="nav-link sidebar-page-title">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
-                  Home Page
+                  Dashboard
                   <i class="right fas fa-angle-left"></i>
                 </p>
               </a>
@@ -45,6 +46,7 @@
             </li>
             <li class="nav-item sidebar-page-header" id="contact-page-header">
               <a href="#" class="nav-link sidebar-page-title">
+              <i class="nav-icon fas fa-envelope"></i>
                 <p>
                   Contact
                   <i class="right fas fa-angle-left"></i>
@@ -61,6 +63,7 @@
             </li>
             <li class="nav-item sidebar-page-header" id="users-page-header">
               <a href="#" class="nav-link sidebar-page-title">
+              <i class="nav-icon fas fa-user"></i>
                 <p>
                   Users
                   <i class="right fas fa-angle-left"></i>

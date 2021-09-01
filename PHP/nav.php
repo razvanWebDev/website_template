068fb5a28d737a8f8ef81777d75e9182df8be849
razvanWebDev <crsn_razvan@yahoo.com>
2021-09-01 16:28:45 +0300

    dynamic current link

--- PHP/nav.php
+++ PHP/nav.php
@@ -1,3 +1,6 @@
+<?php
+    $currentPageName = pathinfo(basename($_SERVER['PHP_SELF']), PATHINFO_FILENAME);
+?>
 <header>
     <a href="/" id="logo-section">
         <img id="logo" src="img/logo.png" alt="Logo">
@@ -6,9 +9,9 @@
     <hr>
     <nav class="header-menu">
         <ul class="nav-links">
-            <li><a href="index">Home</a></li>
-            <li><a href="about">About</a></li>
-            <li><a href="contact">Contact</a></li>
+            <li class="<?php echo $currentPageName == "index" ? "current-page" : ""; ?>"><a href="index">Home</a></li>
+            <li class="<?php echo $currentPageName == "about" ? "current-page" : ""; ?>"><a href="about">About</a></li>
+            <li class="<?php echo $currentPageName == "contact" ? "current-page" : ""; ?>"><a href="contact">Contact</a></li>
         </ul>
     </nav>
     <div id="hamburger">
